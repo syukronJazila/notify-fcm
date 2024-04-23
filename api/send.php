01bced415b7a12be70d6140d8e58f8a63f84f8b2
Muhammad Syukron Jazila <139365360+syukronJazila@users.noreply.github.com>
2024-04-23 20:37:11 +0700

    Add files via upload

---
+++ api/send.php
@@ -0,0 +1,45 @@
+<?php
+
+use Google\Auth\Credentials\ServiceAccountCredentials;
+use Google\Auth\HttpHandler\HttpHandlerFactory;
+
+require 'vendor\autoload.php';
+
+$credential = new ServiceAccountCredentials(
+    "https://www.googleapis.com/auth/firebase.messaging",
+    json_decode(file_get_contents("pvKey.json"), true)
+);
+
+$token = $credential->fetchAuthToken(HttpHandlerFactory::build());
+
+$ch = curl_init("https://fcm.googleapis.com/v1/projects/flodec-p24s/messages:send");
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'Content-Type: application/json',
+    'Authorization: Bearer '.$token['access_token']
+]);
+
+    $payload = json_encode([
+        "message" => [
+            "topic" => "notif",
+            "notification" => [
+                "body" => "Hujan deras terjadi di Jl. Gaperta Ujung, 10 menit lagi akan banjir nih",
+                "title" => "Flodecs"
+            ],
+            "android" => [
+                "notification" => [
+                  "icon" => "baseline_info_24"
+                ]
+            ],
+        ]
+    ]);
+
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+    
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    
+    $response = curl_exec($ch);
+    
+    echo $response;
+
+curl_close($ch);
